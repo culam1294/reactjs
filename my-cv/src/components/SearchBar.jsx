167058ebdd531251babbf97fbd86b971248a89ef
Nguyễn Lam <lamn@ini.local>
2020-03-29 11:25:11 +0700

    thay doi lan 1

---
+++ my-cv/src/components/SearchBar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function SearchBar() {
+  return (
+    <div>
+      <form>
+        <input type="text" placeholder="Search..." />
+        <button>Search</button>
+      </form>
+    </div>
+  );
+}
---
+++ my-cv/src/components/Table.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+export default function Table() {
+  return (
+    <div>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Username</th>
+            <th>Password</th>
+            <th>Permission</th>
+            <th>Sexual</th>
+            <th>Tel</th>
+            
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
