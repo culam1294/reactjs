b41b44d7963f70c0dce5b9e07abd2b751978aa27
Nguyễn Lam <lamn@lam.local>
2020-04-22 16:38:07 +0700

    Updates

---
+++ precache-manifest.674d26d8836190272502dc76d5f9b9c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bef81173270465f0d18e6a8313c969db",
+    "url": "/react-redux/index.html"
+  },
+  {
+    "revision": "044b3ff486165ee804e8",
+    "url": "/react-redux/static/css/2.a43b59a1.chunk.css"
+  },
+  {
+    "revision": "e4d04f940420ce0bdad0",
+    "url": "/react-redux/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "044b3ff486165ee804e8",
+    "url": "/react-redux/static/js/2.36d3a68a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-redux/static/js/2.36d3a68a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4d04f940420ce0bdad0",
+    "url": "/react-redux/static/js/main.a63dbb6f.chunk.js"
+  },
+  {
+    "revision": "b8ed16d6ed1a1f2a3b97",
+    "url": "/react-redux/static/js/runtime-main.00967697.js"
+  }
+]);
